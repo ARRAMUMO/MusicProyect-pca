fd5881cda7d0c6bde2e932c36c7a2c3496accc0d
arramumo <arramumo@.com>
2024-07-24 11:24:48 -0500

    tema 3. actividad

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slides = [
+    {
+      title:" titulo de slide 1",
+      icon: "caret-forward-circle-outline",
+      avatar: "https://thumbs.dreamstime.com/z/avatar-de-dj-que-juega-la-m%C3%BAsica-gr%C3%A1fico-73243163.jpg",
+      image:"assets/imagenes/burro con gafas 1.jpg",
+      description: "Texto del slide 1"
+
+    },
+    {
+      title:" titulo de slide 2",
+      icon: "caret-forward-circle-outline",
+      avatar: "https://thumbs.dreamstime.com/z/avatar-de-dj-que-juega-la-m%C3%BAsica-gr%C3%A1fico-73243163.jpg",
+      image:"assets/imagenes/burro con gafas 1.jpg",
+      description: "Texto del slide 1"
+
+    }, {
+      title:" titulo de slide 3",
+      icon: "caret-forward-circle-outline",
+      avatar: "https://thumbs.dreamstime.com/z/avatar-de-dj-que-juega-la-m%C3%BAsica-gr%C3%A1fico-73243163.jpg",
+      image:"assets/imagenes/burro con gafas 1.jpg",
+      description: "Texto del slide 1"
+
+    },{
+      title:" MUSIC-IN ",
+      icon: "caret-forward-circle-outline",
+      avatar: "https://thumbs.dreamstime.com/z/avatar-de-dj-que-juega-la-m%C3%BAsica-gr%C3%A1fico-73243163.jpg ",
+      image:"assets/imagenes/burromusical.jpg",
+      description: "MUSIC-IN. Musica mas sonada actual"
+
+    }
+  ]
+
   constructor() {}
 
 }
